f3dc5042054f5c0b7013e809d1549c01cafd7df6
IhorRymar <95086464+IhorRymar@users.noreply.github.com>
2022-11-03 10:26:14 +0200

    2nd proof (add jimp, add avatars img)

--- controllers/auth/avatar.js
+++ controllers/auth/avatar.js
@@ -1,22 +1,32 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 
+const Jimp = require('jimp');
+
 const { User } = require('../../models/user');
-        
-const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars')
-        
+
+const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars');
+
 const avatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
+
   const extencion = originalname.split('.').pop();
-  const filename = `${_id}.${extencion}`
+  const filename = `${_id}.${extencion}`;
+
   const resultUpload = path.join(avatarsDir, filename);
   await fs.rename(tempUpload, resultUpload);
+
+  const image = await Jimp.read(resultUpload);
+  await image.resize(250, 250).writeAsync(resultUpload);
+
   const avatarURL = path.join('avatars', filename);
-  await User.findByIdAndUpdate(_id, { avatarURL })
+
+  await User.findByIdAndUpdate(_id, { avatarURL, image });
+
   res.json({
     avatarURL,
   });
-}
+};
 
-  module.exports = avatar;
+module.exports = avatar;
